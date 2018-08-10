0bcb829264607f1d0ebff55413748a48b1a46e35
mayuran siva <smayuran81@gmail.com>
2018-08-10 22:38:33 +0100

    lesson5notyetcomplete

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -1,16 +1,12 @@
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -48,7 +44,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			 * 
 			 */
 		.formLogin().loginPage("/login").permitAll()
-		.loginProcessingUrl("/doLogin");
+		.loginProcessingUrl("/doLogin")
+		/**
+		 * This specify that logout will come to this point
+		 * 
+		 */
+		.and().logout().permitAll().logoutRequestMatcher(new AntPathRequestMatcher("/doLogout", "GET"))
+		//still need to finish off the lesson 5 with the other options that is available
+		.and()
+		.csrf().disable();
 	} //@formatter:on
 	
 	
